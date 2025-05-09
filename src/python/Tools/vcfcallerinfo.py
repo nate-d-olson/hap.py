dcf9ac2aa13b643ac06f4c214322640d29b0edbe
nate-d-olson <nolson@nist.gov>
2025-05-09 12:03:03 -0400

    following pep8

--- src/python/Tools/vcfcallerinfo.py
+++ src/python/Tools/vcfcallerinfo.py
@@ -9,215 +9,247 @@
 #
 # https://github.com/sequencing/licenses/blob/master/Simplified-BSD-License.txt
 
+"""
+Caller Information Extractor
+
+This module provides functionality for extracting caller and aligner information from VCF and BAM files.
+"""
+
 import itertools
 import json
 import logging
 import os
 import subprocess
 import tempfile
+from typing import Dict, List, Optional, Tuple
 
 
-class CallerInfo(object):
+class CallerInfo:
+    """
+    Class for collecting caller and aligner information.
+
+    Attributes:
+        callers: List of tuples containing (caller, version, parameters)
+        aligners: List of tuples containing (aligner, version, parameters)
+    """
     """Class for collecting caller info and version"""
 
-    def __init__(self):
-        # callers and aligners are stored in tuples of three:
-        # (caller/aligner, version, parameters)
-        self.callers = []
-        self.aligners = []
+    def __init__(self) -> None:
+        """Initialize CallerInfo instance."""
+        self.callers: List[Tuple[str, str, str]] = []
+        self.aligners: List[Tuple[str, str, str]] = []
 
-    def __repr__(self):
-        return (
-            "aligners=["
-            + ",".join(["/".join(xx) for xx in self.aligners])
-            + "] "
-            + "callers=["
-            + ",".join(["/".join(xx) for xx in self.callers])
-            + "]"
-        )
+    def __repr__(self) -> str:
+        """Return string representation of CallerInfo instance."""
+        aligners_str = ",".join("/".join(x) for x in self.aligners)
+        callers_str = ",".join("/".join(x) for x in self.callers)
+        return f"aligners=[{aligners_str}] callers=[{callers_str}]"
 
-    def asDict(self):
-        kvd = ["name", "version", "parameters"]
+    def as_dict(self) -> Dict:
+        """
+        Convert caller and aligner information to dictionary format.
+
+        Returns:
+            Dictionary containing aligners and callers information
+        """
+        key_value_dict = ["name", "version", "parameters"]
         return {
-            "aligners": [dict(y for y in zip(kvd, x)) for x in self.aligners],
-            "callers": [dict(y for y in zip(kvd, x)) for x in self.callers],
+            "aligners": [dict(zip(key_value_dict, x)) for x in self.aligners],
+            "callers": [dict(zip(key_value_dict, x)) for x in self.callers],
         }
 
-    def addVCF(self, vcfname):
-        """Add caller versions from a VCF
-        :param vcfname: VCF file name
+    def add_vcf(self, vcfname: str) -> None:
         """
-        with tempfile.NamedTemporaryFile(delete=False) as tf:
-            tf_name = tf.name
+        Add caller versions from a VCF file.
 
-        vfh = {}
+        Args:
+            vcfname: Path to the VCF file
+
+        Raises:
+            Exception: If vcfhdr2json command fails
+        """
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.close()
+        
         try:
-            sp = subprocess.Popen(
-                "vcfhdr2json '%s' '%s'" % (vcfname, tf_name),
-                shell=True,
+            process = subprocess.Popen(
+                ["vcfhdr2json", vcfname, temp_file.name],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
+                text=True
             )
-            o, e = sp.communicate()
+            stdout, stderr = process.communicate()
 
-            if sp.returncode != 0:
-                raise Exception(f"vcfhdr2json call failed: {o.decode('utf-8')} / {e.decode('utf-8')}")
+            if process.returncode != 0:
+                raise Exception(f"vcfhdr2json call failed: {stdout} / {stderr}")
+
+            with open(temp_file.name, "r", encoding="utf-8") as json_file:
+                header_data = json.load(json_file)
+
+            self._process_vcf_header(header_data)
+
         except Exception as ex:
-            logging.error("Error running vcfhdr2json: %s", ex)
-        else:
-            try:
-                with open(tf_name, "r", encoding="utf-8") as f:
-                    vfh = json.load(f)
-            except json.JSONDecodeError as ex:
-                logging.error("Error parsing JSON from vcfhdr2json: %s", ex)
-            except Exception as ex:
-                logging.error("Error reading JSON file: %s", ex)
+            logging.error(f"Error processing VCF header: {ex}")
         finally:
             try:
-                os.unlink(tf_name)
+                os.unlink(temp_file.name)
             except OSError as e:
                 logging.warning(f"Error removing temp file: {e}")
 
-        cp = ["unknown", "unknown", ""]
+        caller_info = ["unknown", "unknown", ""]
         gatk_callers = ["haplotypecaller", "unifiedgenotyper", "mutect"]
         sent_callers = ["haplotyper"]
         source_found = False
 
-        for hf in vfh["fields"]:
+        for header_field in header_data["fields"]:
             try:
-                k = hf["key"]
-                if k == "source":
+                key = header_field["key"]
+                values = header_field.get("values", {})
+
+                if key == "source":
                     try:
-                        cp[0] = str(hf["values"])
+                        caller_info[0] = str(values)
                     except (ValueError, TypeError) as e:
-                        logging.warning(f"Error parsing value: {e}")
+                        logging.warning(f"Error parsing source value: {e}")
                         continue
-                        cp[0] = hf["value"]
-                    if cp[0].startswith("Platypus_Version_"):
-                        cp[1] = cp[0][len("Platypus_Version_") :]
-                        cp[0] = "Platypus"
+                    
+                    if caller_info[0].startswith("Platypus_Version_"):
+                        caller_info[1] = caller_info[0][len("Platypus_Version_") :]
+                        caller_info[0] = "Platypus"
                     source_found = True
-                elif k == "source_version":
+
+                elif key == "source_version":
                     try:
-                        cp[1] = str(hf["values"])
+                        caller_info[1] = str(values)
                     except (ValueError, TypeError) as e:
-                        logging.warning(f"Error parsing value: {e}")
+                        logging.warning(f"Error parsing version value: {e}")
                         continue
-                        cp[1] = hf["value"]
                     source_found = True
-                elif k == "cmdline":
+
+                elif key == "cmdline":
                     try:
-                        cp[2] = str(hf["values"])
+                        caller_info[2] = str(values)
                     except (ValueError, TypeError) as e:
-                        logging.warning(f"Error parsing value: {e}")
+                        logging.warning(f"Error parsing command line value: {e}")
                         continue
-                        cp[2] = hf["value"]
                     source_found = True
-                elif k == "platypusOptions":
+
+                elif key == "platypusOptions":
                     try:
-                        cp[2] = str(hf["values"])
+                        caller_info[2] = str(values)
                     except (ValueError, TypeError) as e:
-                        logging.warning(f"Error parsing value: {e}")
+                        logging.warning(f"Error parsing Platypus options: {e}")
                         continue
-                        cp[2] = hf["value"]
                     source_found = True
-                elif k == "octopus":
-                    # octopus doesn't add a version
-                    self.callers.append(["octopus", "unknown", str(hf["values"])])
-                elif k.startswith("GATKCommandLine"):
+
+                elif key == "octopus":
+                    self.callers.append(["octopus", "unknown", str(values)])
+
+                elif key.startswith("GATKCommandLine"):
                     caller = "GATK"
                     try:
-                        caller += "-" + hf["values"]["ID"]
+                        caller += "-" + values["ID"]
                     except (ValueError, TypeError) as e:
-                        logging.warning(f"Error parsing value: {e}")
+                        logging.warning(f"Error parsing GATK ID: {e}")
                     else:
                         version = "unknown"
                         try:
-                            version = hf["values"]["Version"]
+                            version = values["Version"]
                         except (ValueError, TypeError) as e:
-                            logging.warning(f"Error parsing value: {e}")
+                            logging.warning(f"Error parsing GATK version: {e}")
                         else:
                             options = ""
                             try:
-                                options = hf["values"]["CommandLineOptions"]
+                                options = values["CommandLineOptions"]
                             except (ValueError, TypeError) as e:
-                                logging.warning(f"Error parsing value: {e}")
+                                logging.warning(f"Error parsing GATK options: {e}")
                             else:
                                 if any(g in caller.lower() for g in gatk_callers):
                                     self.callers.append([caller, version, options])
-                elif k.startswith("SentieonCommandLine"):
+
+                elif key.startswith("SentieonCommandLine"):
                     caller = "Sentieon"
                     try:
-                        caller += "-" + hf["values"]["ID"]
+                        caller += "-" + values["ID"]
                     except (ValueError, TypeError) as e:
-                        logging.warning(f"Error parsing value: {e}")
+                        logging.warning(f"Error parsing Sentieon ID: {e}")
                     else:
                         version = "unknown"
                         try:
-                            version = hf["values"]["Version"]
+                            version = values["Version"]
                         except (ValueError, TypeError) as e:
-                            logging.warning(f"Error parsing value: {e}")
+                            logging.warning(f"Error parsing Sentieon version: {e}")
                         else:
                             if any(s in caller.lower() for s in sent_callers):
                                 self.callers.append([caller, version])
 
             except (OSError, IOError) as e:
-                logging.error(f"Error reading BAM file: {e}")
+                logging.error(f"Error reading header field: {e}")
                 continue
-                pass
-        if source_found:
-            self.callers.append(cp)
 
-    def addBAM(self, bamfile):
-        """Extract aligner information from a BAM file
-        :param bamfile: name of BAM file
+        if source_found:
+            self.callers.append(caller_info)
+
+    def add_bam(self, bamfile: str) -> None:
         """
-        sp = subprocess.Popen(
-            "samtools view -H '%s'" % bamfile,
-            shell=True,
+        Extract aligner information from a BAM file.
+
+        Args:
+            bamfile: Path to the BAM file
+
+        Raises:
+            Exception: If samtools command fails
+        """
+        process = subprocess.Popen(
+            ["samtools", "view", "-H", bamfile],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
+            text=True
         )
-        o, e = sp.communicate()
+        stdout, stderr = process.communicate()
 
-        if sp.returncode != 0:
-            raise Exception("Samtools call failed: %s / %s" % (o, e))
+        if process.returncode != 0:
+            raise Exception(f"Samtools call failed: {stdout} / {stderr}")
 
-        for line in o.split("\n"):
+        for line in stdout.split("\n"):
             if not line.startswith("@PG"):
                 continue
+            
             try:
-                # noinspection PyTypeChecker
-                x = dict(y.split(":", 1) for y in line.split("\t")[1:])
+                # Split header line into key-value pairs
+                fields = line.split("\t")[1:]
+                header_dict = dict(field.split(":", 1) for field in fields)
             except (OSError, IOError) as e:
                 logging.error(f"Error reading BAM file: {e}")
+                logging.warning(f"Unable to parse SAM/BAM header line: {line}")
                 continue
-                logging.warn("Unable to parse SAM/BAM header line: %s" % line)
-                continue
-            cp = ["unknown", "unknown", ""]
+            
+            aligner_info = ["unknown", "unknown", ""]
             try:
-                cp[0] = x["PN"]
-            except (OSError, IOError) as e:
-                logging.error(f"Error reading BAM file: {e}")
+                aligner_info[0] = header_dict["PN"]
+            except KeyError as e:
+                logging.warning(f"Missing program name in BAM header: {e}")
                 continue
-                try:
-                    cp[0] = x["ID"]
-                    if "-" in cp[0]:
-                        cp[0] = cp[0].split("-")[0]
-                except (OSError, IOError) as e:
-                logging.error(f"Error reading BAM file: {e}")
-                continue
-                    pass
+            
             try:
-                cp[1] = x["VN"]
-            except (OSError, IOError) as e:
-                logging.error(f"Error reading BAM file: {e}")
-                continue
-                pass
+                aligner_info[1] = header_dict["VN"]
+            except KeyError as e:
+                logging.warning(f"Missing version in BAM header: {e}")
+            
             try:
-                cp[2] = x["CL"]
-            except (OSError, IOError) as e:
-                logging.error(f"Error reading BAM file: {e}")
+                aligner_info[2] = header_dict["CL"]
+            except KeyError as e:
+                logging.warning(f"Missing command line in BAM header: {e}")
+            
+            self.aligners.append(tuple(aligner_info))
+
+            try:
+                aligner_info[0] = header_dict["ID"]
+                if "-" in aligner_info[0]:
+                    aligner_info[0] = aligner_info[0].split("-")[0]
+            except KeyError as e:
+                logging.warning(f"Missing ID in BAM header: {e}")
+                continue
                 continue
                 pass
 
