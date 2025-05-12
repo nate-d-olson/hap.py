0ed0ec613b91e521cc0d449fb21fe4e8e28c7e5b
nate-d-olson <nolson@nist.gov>
2025-05-12 13:57:25 -0400

    WIP: updating project structure

---
+++ src/happy/__init__.py
@@ -0,0 +1,9 @@
+"""
+hap.py - Haplotype comparison tools for benchmarking variant calls.
+
+A bioinformatics tool for benchmarking small variant calls.
+"""
+
+from .version import __version__
+
+__all__ = ["__version__"]
---
+++ src/happy/tools/__init__.py
@@ -0,0 +1,11 @@
+"""
+Tools module for hap.py.
+
+Contains common utilities and helper functions.
+"""
+
+# Import important tools for easier access
+from . import io
+from . import vcf
+from . import bedintervaltree
+from . import bgzip
